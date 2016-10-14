5f5944e34cdc1884264531e79a26765d95182e3d
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2016-10-14 15:07:48 +0100

    Logging improvements

--- app/uk/gov/hmrc/fsetlaunchpadgateway/controllers/CallbackController.scala
+++ app/uk/gov/hmrc/fsetlaunchpadgateway/controllers/CallbackController.scala
@@ -11,12 +11,12 @@ object CallbackController extends CallbackController
 
 trait CallbackController extends FrontendController {
   def present(): Action[AnyContent] = Action.async { implicit request =>
-    Logger.info("Received callback => " + request.body + "\n")
+    Logger.info("Received callback => " + request.body.asText.getOrElse("No callback body detected!") + "\n")
 
-    Logger.info(s"*** content-type: ${request.contentType}")
-    Logger.info(s"*** headers: ${request.headers}")
-    Logger.info(s"*** body: ${request.body}")
-    Logger.info(s"*** query string: ${request.rawQueryString}")
+    Logger.debug(s"*** Content-type: ${request.contentType}" +
+    s"*** Headers: ${request.headers}" +
+    s"*** Body: ${request.body}" +
+    s"*** Query string: ${request.rawQueryString}")
 
     // 1 in 10 calls will be a 500, just to test the retry
     if (Random.shuffle(1 to 10).head == 10) {
