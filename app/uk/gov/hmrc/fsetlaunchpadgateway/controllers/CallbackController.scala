978f7b9de91ec828c0c983e96dafc29be7e358dc
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2016-10-24 07:54:01 +0100

    All callbacks now return a 500

--- app/uk/gov/hmrc/fsetlaunchpadgateway/controllers/CallbackController.scala
+++ app/uk/gov/hmrc/fsetlaunchpadgateway/controllers/CallbackController.scala
@@ -21,7 +21,7 @@ trait CallbackController extends FrontendController {
       s"*** Query string: ${request.rawQueryString}")
 
     // 1 in 10 calls will be a 500, just to test the retry
-    if (Random.shuffle(1 to 10).head == 10) {
+    if (true) {
       Logger.info("Returned a random error on purpose")
       Future.successful(InternalServerError("A purposeful error to test retries occurred!!!![]*$:-()foo.exception\n\n\n "))
     } else {
@@ -44,6 +44,28 @@ trait CallbackController extends FrontendController {
 // scalastyle:off
 /*
   Example callback output, for reference:
-  {"app":"fset-launchpad-gateway","hostname":"public-app-13-fset-launchpad-gateway","timestamp":"2016-08-22 15:23:44.331+00:00","message":"Received callback => AnyContentAsJson({\"candidate_id\":\"cnd_4bf18c2e67fbdfe122614c894403ad1a\",\"custom_candidate_id\":\"CUSTOM_CSR_ID_TO_KNOW_WHO_IS_WHO\",\"interview_id\":13755,\"custom_interview_id\":null,\"custom_invite_id\":\"CSR_CUSTOM_INVITE_REFERENCE\",\"status\":\"final\"})\n\nWith static key = foobar123123123123123123123123123","logger":"application","thread":"play-akka.actor.default-dispatcher-557","level":"INFO","application.home":"/app/fset-launchpad-gateway-0.3.0"}
+  {timestamp":"2016-08-22 15:23:44.331+00:00","message":"Received callback => AnyContentAsJson({\"candidate_id\":\"cnd_4bf18c2e67fbdfe122614c894403ad1a\",\"custom_candidate_id\":\"CUSTOM_CSR_ID_TO_KNOW_WHO_IS_WHO\",\"interview_id\":13755,\"custom_interview_id\":null,\"custom_invite_id\":\"CSR_CUSTOM_INVITE_REFERENCE\",\"status\":\"final\"})\n\n","logger":"application","thread":"play-akka.actor.default-dispatcher-557","level":"INFO",}
  */
 // scalastyle:on
+
+/*
+{"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"finished","deadline":"2016-10-28"}
+
+{"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"final","deadline":"2016-10-28"}
+
+{"candidate_id":"cnd_9b0b1fc7fd81329e7109d217108c6452","custom_candidate_id":"FSCND-6bc83ac4-e547-41f4-8825-5661d5d85a56","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2f2ed237-f0a1-4c04-bc53-536dae436425","status":"question","deadline":"2016-10-28","question_number":"3"}
+
+{"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"question","deadline":"2016-10-28","question_number":"4"}
+
+{"candidate_id":"cnd_9b0b1fc7fd81329e7109d217108c6452","custom_candidate_id":"FSCND-6bc83ac4-e547-41f4-8825-5661d5d85a56","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2f2ed237-f0a1-4c04-bc53-536dae436425","status":"question","deadline":"2016-10-28","question_number":"2"}
+
+{"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"question","deadline":"2016-10-28","question_number":"3"}
+
+{"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"question","deadline":"2016-10-28","question_number":"2"}
+
+{"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"question","deadline":"2016-10-28","question_number":"1"}
+
+{"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"view_practice_question","deadline":"2016-10-28"}
+
+{"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"setup_process","deadline":"2016-10-28"}
+ */
