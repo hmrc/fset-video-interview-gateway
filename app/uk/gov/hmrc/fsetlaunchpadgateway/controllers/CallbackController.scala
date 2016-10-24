34d115fc64ce6036537390e617715cb69bd07fa9
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2016-10-24 08:04:58 +0100

    Disable scalastyle for comment lines

--- app/uk/gov/hmrc/fsetlaunchpadgateway/controllers/CallbackController.scala
+++ app/uk/gov/hmrc/fsetlaunchpadgateway/controllers/CallbackController.scala
@@ -46,7 +46,6 @@ trait CallbackController extends FrontendController {
   Example callback output, for reference:
   {timestamp":"2016-08-22 15:23:44.331+00:00","message":"Received callback => AnyContentAsJson({\"candidate_id\":\"cnd_4bf18c2e67fbdfe122614c894403ad1a\",\"custom_candidate_id\":\"CUSTOM_CSR_ID_TO_KNOW_WHO_IS_WHO\",\"interview_id\":13755,\"custom_interview_id\":null,\"custom_invite_id\":\"CSR_CUSTOM_INVITE_REFERENCE\",\"status\":\"final\"})\n\n","logger":"application","thread":"play-akka.actor.default-dispatcher-557","level":"INFO",}
  */
-// scalastyle:on
 
 /*
 {"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"finished","deadline":"2016-10-28"}
@@ -69,3 +68,5 @@ trait CallbackController extends FrontendController {
 
 {"candidate_id":"cnd_222670c903da0bf709660ec3129ccdf6","custom_candidate_id":"FSCND-7292eea2-57f0-4d25-afc7-fd605c9388f9","interview_id":13917,"custom_interview_id":null,"custom_invite_id":"FSINV-2d442182-c7f4-4380-9187-8b9d8b852a19","status":"setup_process","deadline":"2016-10-28"}
  */
+
+// scalastyle:on
