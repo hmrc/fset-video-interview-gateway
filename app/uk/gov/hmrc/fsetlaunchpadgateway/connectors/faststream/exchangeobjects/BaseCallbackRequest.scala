e9652b68144faa0958e70b0fbd4fd4be34c94de0
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2016-11-10 11:52:49 +0000

    Compile fix

--- app/uk/gov/hmrc/fsetlaunchpadgateway/connectors/faststream/exchangeobjects/BaseCallbackRequest.scala
+++ app/uk/gov/hmrc/fsetlaunchpadgateway/connectors/faststream/exchangeobjects/BaseCallbackRequest.scala
@@ -1,6 +1,6 @@
 package uk.gov.hmrc.fsetlaunchpadgateway.connectors.faststream.exchangeobjects
 
-import org.joda.time.LocalDate
+import org.joda.time.{ DateTime, LocalDate }
 
 abstract class BaseCallbackRequest(received: DateTime, candidateId: String, customCandidateId: String, interviewId: Int,
   customInterviewId: Option[String], customInviteId: String, status: String, deadline: LocalDate)
