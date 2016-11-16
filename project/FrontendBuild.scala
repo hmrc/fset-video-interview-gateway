3f3ae3f990506c57b382348f8c5888f8cf54a3d2
Ben Stephenson <bjames.stephenson@gmail.com>
2016-11-16 10:27:10 +0000

    Bump play auth, partials, micro service and front end bootstrap versions and remove unsued vals.

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -11,23 +11,13 @@ object FrontendBuild extends Build with MicroService {
 }
 
 private object Versions {
-  val microserviceBootstrapVersion  = "4.2.0"
-  val ficus                         = "1.1.2"
-  val cacheClient                   = "5.3.0"
-  val frontend                      = "6.4.0"
-  val playConfig                    = "2.0.1"
-  val playHealth                    = "1.1.0"
-  val urlBuilder                    = "1.0.0"
-  val httpclient                    = "4.3.4"
-  val jsonLogger                    = "2.1.1"
-  val passcode                      = "3.2.0"
+  val microserviceBootstrapVersion  = "4.4.0"
   val scalatest                     = "2.2.2"
   val pegdown                       = "1.4.2"
   val jsoup                         = "1.7.3"
   val wiremock                      = "1.57"
   val hmrctest                      = "1.4.0"
   val scalatestplus                 = "1.2.0"
-  val silhouette                    = "2.0.2"
 }
 
 private object AppDependencies {
@@ -39,11 +29,11 @@ private object AppDependencies {
   private val playJsonLoggerVersion = "2.1.1"
   private val frontendBootstrapVersion = "6.6.0"
   private val playUiVersion = "4.16.0"
-  private val playPartialsVersion = "4.4.0"
-  private val playAuthorisedFrontendVersion = "5.4.0"
+  private val playPartialsVersion = "4.5.0"
+  private val playAuthorisedFrontendVersion = "5.5.0"
   private val playConfigVersion = "2.1.0"
   private val playWhitelist = "1.1.0"
-  
+
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap"   % microserviceBootstrapVersion,
@@ -76,5 +66,3 @@ private object AppDependencies {
 
   def apply(): Seq[ModuleID] = compile ++ Test()
 }
-
-
