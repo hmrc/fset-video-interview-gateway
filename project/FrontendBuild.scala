53e5962aa7f2f047aa644812f1c63ebf6160a00f
Ian Stainer <ian.stainer@valtech.co.uk>
2018-10-03 13:47:53 +0100

    fset-2163 file changes to work with new build environment (includes library tidy up)

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -11,30 +11,27 @@ object FrontendBuild extends Build with MicroService {
 }
 
 private object Versions {
+  val hmrcFrontendBootstrapVersion      = "10.4.0"
   val hmrcMicroserviceBootstrapVersion  = "8.3.0"
-  //val pegdown                       = "1.5.0"
- // val jsoup                         = "1.11.3"
-  val wiremock                      = "2.19.0"
-  val hmrctest                      = "3.1.0"
-  val scalatestplus                 = "1.5.1"
+  val hmrcPlayPartialsVersion           = "6.1.0"
+  val hmrcPlayAuthorisedFrontendVersion = "7.0.0"
+  val hmrcPlayWhitelistVersion          = "2.0.0"
+
+  val hmrctest                          = "2.4.0"
+  val scalatestplus                     = "1.5.1"
 }
 
 private object AppDependencies {
   import play.sbt.PlayImport._
   import Versions._
 
-  private val frontendBootstrapVersion = "10.4.0"
-  private val playPartialsVersion = "6.1.0"
-  private val playAuthorisedFrontendVersion = "7.0.0"
-  private val playWhitelist = "2.0.0"
-
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap"   % hmrcMicroserviceBootstrapVersion,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
-    "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
-    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion,
-    "uk.gov.hmrc" %% "play-whitelist-filter" % playWhitelist
+    "uk.gov.hmrc" %% "frontend-bootstrap"       % hmrcFrontendBootstrapVersion,
+    "uk.gov.hmrc" %% "play-partials"            % hmrcPlayPartialsVersion,
+    "uk.gov.hmrc" %% "play-authorised-frontend" % hmrcPlayAuthorisedFrontendVersion,
+    "uk.gov.hmrc" %% "play-whitelist-filter"    % hmrcPlayWhitelistVersion
   )
 
   trait TestDependencies {
@@ -46,9 +43,6 @@ private object AppDependencies {
       lazy val test = Seq(
         "org.mockito" % "mockito-all" % "1.10.19",
         "org.scalatestplus.play"    %% "scalatestplus-play"       % scalatestplus % "test",
-        //"org.pegdown"               %  "pegdown"                  % pegdown       % "test",
-        //"org.jsoup"                 %  "jsoup"                    % jsoup         % "test",
-        "com.github.tomakehurst"    %  "wiremock"                 % wiremock      % "test",
         "uk.gov.hmrc"               %% "hmrctest"                 % hmrctest      % "test"
       )
     }.test
